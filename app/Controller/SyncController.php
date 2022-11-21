00e3ac95682bd9a6e3027d3e6a71c5502f02188f
Abdul Hafidz Anshari <aditans88@gmail.com>
2022-11-21 20:16:14 +0700

    update

--- app/Controller/SyncController.php
+++ app/Controller/SyncController.php
@@ -4,22 +4,56 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
+use Hyperf\Validation\Contract\ValidatorFactoryInterface;
 
 #[Controller]
 class SyncController
 {
+    #[Inject]
+    protected ValidatorFactoryInterface $validator;
 
     #[RequestMapping(methods: "POST", path: "/sync/{truckId}")]
-    public function receive(RequestInterface $request, ResponseInterface $response)
+    public function receive($truckId, RequestInterface $request, ResponseInterface $response)
     {
+        $this->validator->make($request->all(), [
+            'meta.site' => 'required',
+            'meta.pit' => 'required',
+            'meta.ewacs_location' => 'required',
+            'data' => 'required|array'
+        ])->validate();
+
         $meta = $request->input('meta', null);
         $data = $request->input('data', []);
 
         $total = count($data);
+
+        $site = Site::updateOrCreate([
+            'site' => $meta['site'],
+            'pit'  => $meta['pit'],
+            'ewacs_location' => $meta['ewacs_location']
+        ], [
+            'burden' => $meta['burden'] ?? 0,
+            'spacing' => $meta['spacing'] ?? 0
+        ]);
+
+        foreach($data as $row) {
+            Hole::updateOrCreate([
+                'site_id' => $site->id,
+                'truck_id' => $truckId,
+                'hole_code' => $row['hole_code']
+            ], [
+                'deep' => $row['deep'],
+                'volume' => $row['volume'],
+                'condition' => $row['condition']
+            ]);
+        }
+
+        $total = $site->holes()->count();
 
         return $response->json([
             'success' => true, 
